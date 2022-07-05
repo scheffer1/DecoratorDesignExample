44dca5af762da4a1f9699224cf07435b2c86429b
MATEUS SCHEFFERm <mateusscheffer13@gmail.com>
2022-07-05 18:13:05 -0300

    Initial commit

---
+++ Decorator/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sorvete s = new Sorvete();
+
+
+            Console.WriteLine("Sorvete:");
+            Console.WriteLine("{0:c}", s.Preco);
+
+
+            Console.WriteLine();
+
+
+            SorveteComCobertura c;
+            c = new SorveteComCobertura(s);
+
+
+            Console.WriteLine("Sorvete com cobertura:");
+            Console.WriteLine("{0:c}", c.Preco);
+
+
+            Console.WriteLine();
+
+
+            SorveteComBalinha b;
+            b = new SorveteComBalinha(s);
+
+            Console.WriteLine("Sorvete com balinha:");
+            Console.WriteLine("{0:c}", b.Preco);
+
+
+            Console.WriteLine();
+
+
+            SorveteComCobertura cb;
+            cb = new SorveteComCobertura(b);
+
+
+            Console.WriteLine("Sorvete com cobertura E balinha:");
+            Console.WriteLine("{0:c}", cb.Preco);
+
+
+            Console.ReadKey();
+        }
+    }
+
+
+    //IComponent
+    interface ISorvete
+    {
+        //State
+        double Preco { get; }
+    }
+
+
+    //Component Não HERDÁVEL
+    sealed class Sorvete : ISorvete
+    {
+        //State
+        public double Preco
+        {
+            get
+            {
+                return 1.50;
+            }
+        }
+    }
+
+
+    //Decorator
+    class SorveteComCobertura : ISorvete
+    {
+        //Component : IComponent
+        ISorvete s;
+
+
+        public SorveteComCobertura(ISorvete s)
+        {
+            this.s = s;
+        }
+
+
+        //State
+        public double Preco
+        {
+            get
+            {
+                return this.s.Preco + 0.50;
+            }
+        }
+    }
+
+
+    //Decorator
+    class SorveteComBalinha : ISorvete
+    {
+        //Component : IComponent
+        ISorvete s;
+
+
+        public SorveteComBalinha(ISorvete s)
+        {
+            this.s = s;
+        }
+
+
+        //State
+        public double Preco
+        {
+            get
+            {
+                return this.s.Preco + 0.75;
+            }
+        }
+    }
+}
